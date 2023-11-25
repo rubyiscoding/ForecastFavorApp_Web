909a79db3cb9e0a6ba28b65b8b9faba5dec4e7ae
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-24 20:47:17 -0500

    Initial commit of ASP.NET Core 6 MVC project

---
+++ ForecastFavorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ForecastFavorApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
