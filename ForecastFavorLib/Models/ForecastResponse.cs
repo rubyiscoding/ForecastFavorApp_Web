b4e3266c82f9b3e521a63bf473046d69f8a52c7c
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-24 22:28:59 -0500

    Add ForecastResponse model for weather forecast data encapsulation

---
+++ ForecastFavorLib/Models/ForecastResponse.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ForecastFavorLib.Models
+{
+    public class ForecastResponse
+    {
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("current")]
+        public CurrentWeather Current { get; set; }
+
+        [JsonProperty("forecast")]
+        public Forecast Forecast { get; set; }
+    }
+
+    public class Forecast
+    {
+        [JsonProperty("forecastday")]
+        public List<ForecastDay> ForecastDay { get; set; }
+    }
+}
