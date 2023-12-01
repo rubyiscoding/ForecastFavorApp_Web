e6ce11acaedcc3861358974bc971b3e96581e0d1
sreenathsegar <A00255694@mycambrian.ca>
2023-12-01 15:30:16 -0500

    Fixed a minor bug in the unit test code

--- ForecastFavorApp-UnitTest/ForecastFavorAppTest.cs
+++ ForecastFavorApp-UnitTest/ForecastFavorAppTest.cs
@@ -1,18 +1,23 @@
 using Newtonsoft.Json;
 using ForecastFavorLib.Models;
 
-namespace ForecastFavorApp_UnitTest;
-
-[TestClass]
-public class ForecastFavorAppTest
+namespace ForecastFavorApp_UnitTest
 {
-    [TestMethod]
-    public void CurrentWeatherResponseTests()
+    [TestClass]
+    public class ForecastFavorAppTest
     {
-        string json = @"{
+        [TestMethod]
+        public void CurrentWeatherResponseTests()
+        {
+            string json = @"{
                 'location': {
-                    'latitude': 40.7128,
-                    'longitude': -74.0060
+                    'name': 'CityName',
+                    'region': 'RegionName',
+                    'country': 'CountryName',
+                    'lat': 40.7128,
+                    'lon': -74.0060,
+                    'tz_id': 'TimeZoneId',
+                    'localtime': '2023-12-01 12:00:00'
                 },
                 'current': {
                     'last_updated': '2023-12-01 12:00:00',
@@ -20,7 +25,8 @@ public class ForecastFavorAppTest
                     'temp_f': 77.9,
                     'condition': {
                         'text': 'Clear',
-                        'icon': 'https://example.com/clear.png'
+                        'icon': 'https://example.com/clear.png',
+                        'code': 800
                     },
                     'pressure_in': 29.92,
                     'humidity': 50,
@@ -29,14 +35,38 @@ public class ForecastFavorAppTest
                     'wind_kph': 10.0
                 }
             }";
-        CurrentWeatherResponse response = JsonConvert.DeserializeObject<CurrentWeatherResponse>(json);
-        Assert.IsNotNull(response);
-        Assert.IsNotNull(response.Location);
-        Assert.IsNotNull(response.Current);
-        Assert.AreEqual(40.7128, response.Location.Latitude);
-        Assert.AreEqual(-74.0060, response.Location.Longitude);
-        Assert.AreEqual("2023-12-01 12:00:00", response.Current.LastUpdated);
-        Assert.AreEqual(25.5, response.Current.TemperatureC);
-        Assert.AreEqual(77.9, response.Current.TemperatureF);
+
+            try
+            {
+                CurrentWeatherResponse response = JsonConvert.DeserializeObject<CurrentWeatherResponse>(json);
+                Assert.IsNotNull(response);
+                Assert.IsNotNull(response.Location);
+                Assert.IsNotNull(response.Current);
+                Assert.AreEqual("CityName", response.Location?.Name);
+                Assert.AreEqual("RegionName", response.Location?.Region);
+                Assert.AreEqual("CountryName", response.Location?.Country);
+                Assert.AreEqual(40.7128, response.Location?.Latitude);
+                Assert.AreEqual(-74.0060, response.Location?.Longitude);
+                Assert.AreEqual("TimeZoneId", response.Location?.TimeZoneId);
+                Assert.AreEqual("2023-12-01 12:00:00", response.Location?.LocalTime);
+                Assert.AreEqual("2023-12-01 12:00:00", response.Current.LastUpdated);
+                Assert.AreEqual(25.5, response.Current.TemperatureC);
+                Assert.AreEqual(77.9, response.Current.TemperatureF);
+                Assert.IsNotNull(response.Current.Condition);
+                Assert.AreEqual("Clear", response.Current.Condition.Text);
+                Assert.AreEqual("https://example.com/clear.png", response.Current.Condition.Icon);
+                Assert.AreEqual(800, response.Current.Condition.Code);
+                Assert.AreEqual(29.92, response.Current.PressureIn);
+                Assert.AreEqual(50, response.Current.Humidity);
+                Assert.AreEqual(20, response.Current.Cloud);
+                Assert.AreEqual(26.0, response.Current.FeelsLikeC);
+                Assert.AreEqual(10.0, response.Current.WindKph);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+                throw;
+            }
+        }
     }
 }
